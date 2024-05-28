bcba053d5668d9f25e50d4d8e0b409fd9ba472f5
RomanGorkavenko <roman@gorkavenko.ru>
2024-05-28 20:23:44 +0300

    init commit

---
+++ src/main/java/ru/ideaplatform/tickets/Main.java
@@ -0,0 +1,7 @@
+package ru.ideaplatform.tickets;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/ideaplatform/tickets/model/Ticket.java
@@ -0,0 +1,4 @@
+package ru.ideaplatform.tickets.model;
+
+public class Ticket {
+}
